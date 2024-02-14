ef99889e9fdf4c8b71ccb4033cb129409c52df11
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-13 23:56:34 -0600

    Check data

--- prepare_data.py
+++ prepare_data.py
@@ -18,3 +18,13 @@ import random
 home_dir = os.path.expanduser('~')
 data_dir = os.path.join(home_dir, 'data')
 work_dir = os.path.join(data_dir, 'landcover-ai')
+img_dir = os.path.join(work_dir, 'images')
+mask_dir = os.path.join(work_dir, 'masks')
+
+temp_img = cv2.imread(os.path.join(img_dir, 'M-34-51-C-d-4-1.tif'))
+plt.imshow(temp_img[:,:,2])
+plt.savefig("figure01.png")
+plt.close()
+temp_mask = cv2.imread(os.path.join(mask_dir, 'M-34-51-C-d-4-1.tif'))
+labels, count = np.unique(temp_mask[:,:,0], return_counts=True)
+print("Labels are: ", labels, " and the counts are: ", count)
