34c66c20bab34be67ec31a2205cc5604aad73f15
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-14 21:50:41 -0600

    Original version

--- train_model.py
+++ train_model.py
@@ -1,3 +1,35 @@
+# https://youtu.be/0W6MKZqSke8
+"""
+Author: Dr. Sreenivas Bhattiprolu
+
+This code can be used to perform semantic segmentation of aerial / satellite
+images, especially the dataset from landcover.ai
+
+Dataset from : https://landcover.ai/
+Dataset description: https://arxiv.org/pdf/2005.02264.pdf
+
+Link doesn't seem to work anymore: Try this one instead: https://landcover.ai.linuxpolska.com/
+
+labels:
+    0: Unlabeled background
+    1: Buildings
+    2: Woodlands
+    3: Water
+
+You can use any U-net but this code demonstrates the use of pretrained encoder
+in the U-net - available as part of segmentation models library.
+
+To install the segmentation models library: pip install -U segmentation-models
+If you are running into generic_utils error when loading segmentation models library
+watch this video to fix it: https://youtu.be/syJZxDtLujs
+
+Use the landcover_prepare_data.py to prepare your data.
+e.g., divide images to smaller patches, svae only the patches with real labels, split to train and val.
+
+
+
+"""
+
 import os
 import cv2
 import numpy as np
@@ -6,27 +38,29 @@ import segmentation_models as sm
 from tensorflow.keras.metrics import MeanIoU
 import random
 
-home_dir = os.path.expanduser('~')
-data_dir = os.path.join(home_dir, 'data')
-work_dir = os.path.join(data_dir, 'landcover-ai')
+# tf.compat.v1.disable_eager_execution() #in case the model gets very slow, may be due to a bug in TF2.0. Uncomment this.
+# https://github.com/tensorflow/tensorflow/issues/33024
 
-train_img_dir = os.path.join(work_dir, 'results', 'train', 'images')
-train_mask_dir = os.path.join(work_dir, 'results', 'train', 'masks')
+# Also check this in case you notice training to be getting increasingly slow each epoch.
+# https://stackoverflow.com/questions/53683164/keras-occupies-an-indefinitely-increasing-amount-of-memory-for-each-epoch
+
+################################################################
+# Get an understanding by looking at a few random images and masks
+
+train_img_dir = "data/data_for_keras_aug/train_images/train/"
+train_mask_dir = "data/data_for_keras_aug/train_masks/train/"
 
 img_list = os.listdir(train_img_dir)
 msk_list = os.listdir(train_mask_dir)
 
 num_images = len(os.listdir(train_img_dir))
-img_num = random.randint(0, num_images-1)
 
-img_path = os.path.join(train_img_dir, img_list[img_num])
-print(img_path)
-img_for_plot = cv2.imread(img_path, 1)
+img_num = random.randint(0, num_images - 1)
+
+img_for_plot = cv2.imread(train_img_dir + img_list[img_num], 1)
 img_for_plot = cv2.cvtColor(img_for_plot, cv2.COLOR_BGR2RGB)
 
-mask_path = os.path.join(train_mask_dir, msk_list[img_num])
-print(mask_path)
-mask_for_plot =cv2.imread(mask_path, 0)
+mask_for_plot = cv2.imread(train_mask_dir + msk_list[img_num], 0)
 
 plt.figure(figsize=(12, 8))
 plt.subplot(121)
@@ -35,30 +69,41 @@ plt.title('Image')
 plt.subplot(122)
 plt.imshow(mask_for_plot, cmap='gray')
 plt.title('Mask')
-plt.savefig('figure03.png')
-plt.close()
+plt.show()
 
-# Define data generator
-seed=24
-batch_size=16
-n_classes=4
+################################################################
+
+# Define Generator for images and masks so we can read them directly from the drive.
+
+seed = 24
+batch_size = 16
+n_classes = 4
 
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-from tensorflow.keras.utils import to_categorical
 
+scaler = MinMaxScaler()
+from keras.utils import to_categorical
+
+# Use this to preprocess input for transfer learning
 BACKBONE = 'resnet34'
 preprocess_input = sm.get_preprocessing(BACKBONE)
 
+
+# Define a function to perform additional preprocessing after datagen.
+# For example, scale images, convert masks to categorical, etc.
 def preprocess_data(img, mask, num_class):
     # Scale images
     img = scaler.fit_transform(img.reshape(-1, img.shape[-1])).reshape(img.shape)
-    img = preprocess_input(img)
+    img = preprocess_input(img)  # Preprocess based on the pretrained backbone...
+    # Convert mask to one-hot
     mask = to_categorical(mask, num_class)
 
     return (img, mask)
 
 
+# Define the generator.
+# We are not doing any rotation or zoom to make sure mask values are not interpolated.
+# It is important to keep pixel values in mask as 0, 1, 2, 3, .....
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
@@ -89,77 +134,84 @@ def trainGenerator(train_img_path, train_mask_path, num_class):
         img, mask = preprocess_data(img, mask, num_class)
         yield (img, mask)
 
-train_img_path = os.path.join(work_dir, 'results', 'train', 'images')
-train_mask_path = os.path.join(work_dir, 'results', 'train', 'masks')
+
+train_img_path = "data/data_for_keras_aug/train_images/"
+train_mask_path = "data/data_for_keras_aug/train_masks/"
 train_img_gen = trainGenerator(train_img_path, train_mask_path, num_class=4)
 
-val_img_path = os.path.join(work_dir, 'results', 'val', 'images')
-val_mask_path = os.path.join(work_dir, 'results', 'val', 'masks')
+val_img_path = "data/data_for_keras_aug/val_images/"
+val_mask_path = "data/data_for_keras_aug/val_masks/"
 val_img_gen = trainGenerator(val_img_path, val_mask_path, num_class=4)
 
+# Make sure the generator is working and that images and masks are indeed lined up.
+# Verify generator.... In python 3 next() is renamed as __next__()
 x, y = train_img_gen.__next__()
-for i in range(0,3):
+
+for i in range(0, 3):
     image = x[i]
     mask = np.argmax(y[i], axis=2)
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.imshow(image)
-    plt.subplot(1,2,2)
+    plt.subplot(1, 2, 2)
     plt.imshow(mask, cmap='gray')
-    plt.savefig(f"figure0{4+i}.png")
-    plt.close()
+    plt.show()
 
 x_val, y_val = val_img_gen.__next__()
 
-for i in range(0,3):
+for i in range(0, 3):
     image = x_val[i]
     mask = np.argmax(y_val[i], axis=2)
-    plt.subplot(1,2,1)
+    plt.subplot(1, 2, 1)
     plt.imshow(image)
-    plt.subplot(1,2,2)
+    plt.subplot(1, 2, 2)
     plt.imshow(mask, cmap='gray')
-    plt.savefig(f"figure0{7+i}.png")
-    plt.close()
+    plt.show()
 
-num_train_imgs = len(os.listdir(train_img_path))
-num_val_images = len(os.listdir(val_img_path))
-steps_per_epoch = num_train_imgs//batch_size
-val_steps_per_epoch = num_val_images//batch_size
+###########################################################################
+# Define the model metrcis and load model.
+
+num_train_imgs = len(os.listdir('data/data_for_keras_aug/train_images/train/'))
+num_val_images = len(os.listdir('data/data_for_keras_aug/val_images/val/'))
+steps_per_epoch = num_train_imgs // batch_size
+val_steps_per_epoch = num_val_images // batch_size
 
 IMG_HEIGHT = x.shape[1]
-IMG_WIDTH  = x.shape[2]
+IMG_WIDTH = x.shape[2]
 IMG_CHANNELS = x.shape[3]
 
-n_classes=4
+n_classes = 4
 
 #############################################################################
-#Use transfer learning using pretrained encoder in the U-Net
-#(make sure you uncomment the preprocess_input part in the
+# Use transfer learning using pretrained encoder in the U-Net
+# (make sure you uncomment the preprocess_input part in the
 # preprocess_data function above)
 ################################################################
-#Define the model
+# Define the model
 # define model
 model = sm.Unet(BACKBONE, encoder_weights='imagenet',
                 input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS),
                 classes=n_classes, activation='softmax')
 model.compile('Adam', loss=sm.losses.categorical_focal_jaccard_loss, metrics=[sm.metrics.iou_score])
 
-#Other losses to try: categorical_focal_dice_loss, cce_jaccard_loss, cce_dice_loss, categorical_focal_loss
+# Other losses to try: categorical_focal_dice_loss, cce_jaccard_loss, cce_dice_loss, categorical_focal_loss
 
-#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=metrics)
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=metrics)
 print(model.summary())
 print(model.input_shape)
-
-history=model.fit(train_img_gen,
-          steps_per_epoch=steps_per_epoch,
-          epochs=25,
-          verbose=1,
-          validation_data=val_img_gen,
-          validation_steps=val_steps_per_epoch)
+# Fit the model
+# history = model.fit(my_generator, validation_data=validation_datagen, steps_per_epoch=len(X_train) // 16, validation_steps=len(X_train) // 16, epochs=100)
+# Train the model.
+history = model.fit(train_img_gen,
+                    steps_per_epoch=steps_per_epoch,
+                    epochs=25,
+                    verbose=1,
+                    validation_data=val_img_gen,
+                    validation_steps=val_steps_per_epoch)
 
 model.save('landcover_25_epochs_RESNET_backbone_batch16.hdf5')
 
 ##################################################################
-#plot the training and validation IoU and loss at each epoch
+# plot the training and validation IoU and loss at each epoch
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(loss) + 1)
@@ -169,8 +221,7 @@ plt.title('Training and validation loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.legend()
-plt.savefig("figure10.png")
-plt.close()
+plt.show()
 
 acc = history.history['iou_score']
 val_acc = history.history['val_iou_score']
@@ -181,8 +232,7 @@ plt.title('Training and validation IoU')
 plt.xlabel('Epochs')
 plt.ylabel('IoU')
 plt.legend()
-plt.savefig("figure11.png")
-plt.close()
+plt.show()
 
 #####################################################
 
@@ -190,13 +240,13 @@ from keras.models import load_model
 
 model = load_model("landcover_25_epochs_RESNET_backbone_batch16.hdf5", compile=False)
 
-#batch_size=32 #Check IoU for a batch of images
+# batch_size=32 #Check IoU for a batch of images
 
-#Test generator using validation data.
+# Test generator using validation data.
 
 test_image_batch, test_mask_batch = val_img_gen.__next__()
 
-#Convert categorical to integer for visualization and IoU calculation
+# Convert categorical to integer for visualization and IoU calculation
 test_mask_batch_argmax = np.argmax(test_mask_batch, axis=3)
 test_pred_batch = model.predict(test_image_batch)
 test_pred_batch_argmax = np.argmax(test_pred_batch, axis=3)
@@ -207,8 +257,8 @@ IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
 print("Mean IoU =", IOU_keras.result().numpy())
 
 #######################################################
-#View a few images, masks and corresponding predictions.
-img_num = random.randint(0, test_image_batch.shape[0]-1)
+# View a few images, masks and corresponding predictions.
+img_num = random.randint(0, test_image_batch.shape[0] - 1)
 
 plt.figure(figsize=(12, 8))
 plt.subplot(231)
@@ -220,5 +270,5 @@ plt.imshow(test_mask_batch_argmax[img_num])
 plt.subplot(233)
 plt.title('Prediction on test image')
 plt.imshow(test_pred_batch_argmax[img_num])
-plt.savefig("figure11.png")
-plt.close()
+plt.show()
+
