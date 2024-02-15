a878d4f442f330237ce344d6813c653102cfa264
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-14 21:13:56 -0600

    Original version

--- prepare_data.py
+++ prepare_data.py
@@ -1,3 +1,28 @@
+# https://youtu.be/0W6MKZqSke8
+"""
+Author: Dr. Sreenivas Bhattiprolu 
+
+The following code performs these tasks - relevant to work with landcover dataset
+from here: https://landcover.ai/
+
+Code can be modified to work with any other dataset.
+
+Tasks achieved.
+
+1. Read large images and corresponding masks, divide them into smaller patches.
+And write the patches as images to the local drive.  
+
+2. Save only images and masks where masks have some decent amount of labels other than 0. 
+Using blank images with label=0 is a waste of time and may bias the model towards 
+unlabeled pixels. 
+
+3. Divide the sorted dataset from above into train and validation datasets. 
+
+4. You have to manually move some folders and rename appropriately if you want to use 
+ImageDataGenerator from keras. 
+
+"""
+
 import os
 import cv2
 import numpy as np
@@ -14,25 +39,25 @@ import segmentation_models as sm
 from tensorflow.keras.metrics import MeanIoU
 import random
 
-# Configure dirs
-home_dir = os.path.expanduser('~')
-data_dir = os.path.join(home_dir, 'data')
-work_dir = os.path.join(data_dir, 'landcover-ai')
-img_dir = os.path.join(work_dir, 'images')
-mask_dir = os.path.join(work_dir, 'masks')
-
-temp_img = cv2.imread(os.path.join(img_dir, 'M-34-51-C-d-4-1.tif'))
-plt.imshow(temp_img[:,:,2])
-plt.savefig("figure01.png")
-plt.close()
-temp_mask = cv2.imread(os.path.join(mask_dir, 'M-34-51-C-d-4-1.tif'))
-labels, count = np.unique(temp_mask[:,:,0], return_counts=True)
+# Quick understanding of the dataset
+temp_img = cv2.imread("data/images/M-34-51-C-d-4-1.tif")  # 3 channels / spectral bands
+plt.imshow(temp_img[:, :, 2])  # View each channel...
+temp_mask = cv2.imread("data/masks/M-34-51-C-d-4-1.tif")  # 3 channels but all same.
+labels, count = np.unique(temp_mask[:, :, 0], return_counts=True)  # Check for each channel. All chanels are identical
 print("Labels are: ", labels, " and the counts are: ", count)
-os.makedirs(os.path.join(work_dir, "256_patches", "images"), exist_ok=True)
-os.makedirs(os.path.join(work_dir, "256_patches", "masks"), exist_ok=True)
-os.makedirs(os.path.join(work_dir, "256_patches", "images_with_useful_info", "images"), exist_ok=True)
-os.makedirs(os.path.join(work_dir, "256_patches", "images_with_useful_info", "masks"), exist_ok=True)
+
+# Now, crop each large image into patches of 256x256. Save them into a directory
+# so we can use data augmentation and read directly from the drive.
+root_directory = 'data/'
+
 patch_size = 256
+
+# Read images from repsective 'images' subdirectory
+# As all images are of different size we have 2 options, either resize or crop
+# But, some images are too large and some small. Resizing will change the size of real objects.
+# Therefore, we will crop them to a nearest size divisible by 256 and then
+# divide all images into patches of 256x256x3.
+img_dir = root_directory + "images/"
 for path, subdirs, files in os.walk(img_dir):
     # print(path)
     dirname = path.split(os.path.sep)[-1]
@@ -58,13 +83,16 @@ for path, subdirs, files in os.walk(img_dir):
                 for j in range(patches_img.shape[1]):
                     single_patch_img = patches_img[i, j, :, :]
                     # single_patch_img = (single_patch_img.astype('float32')) / 255. #We will preprocess using one of the backbones
-                    single_patch_img = single_patch_img[0]  # Drop the extra unecessary dimension that patchify adds.
+                    single_patch_img = single_patch_img[
+                        0]  # Drop the extra unecessary dimension that patchify adds.
 
-                    cv2.imwrite(os.path.join(work_dir, "256_patches", "images",
-                                             image_name + "patch_" + str(i) + str(j) + ".tif"),
-                                single_patch_img)
+                    cv2.imwrite(root_directory + "256_patches/images/" +
+                                image_name + "patch_" + str(i) + str(j) + ".tif", single_patch_img)
                     # image_dataset.append(single_patch_img)
 
+# Now do the same as above for masks
+# For this specific dataset we could have added masks to the above code as masks have extension png
+mask_dir = root_directory + "masks/"
 for path, subdirs, files in os.walk(mask_dir):
     # print(path)
     dirname = path.split(os.path.sep)[-1]
@@ -90,25 +118,23 @@ for path, subdirs, files in os.walk(mask_dir):
                     single_patch_mask = patches_mask[i, j, :, :]
                     # single_patch_img = (single_patch_img.astype('float32')) / 255. #No need to scale masks, but you can do it if you want
                     # single_patch_mask = single_patch_mask[0] #Drop the extra unecessary dimension that patchify adds.
-                    cv2.imwrite(os.path.join(work_dir, "256_patches", "masks",
-                                             mask_name + "patch_" + str(i) + str(j) + ".tif"),
-                                single_patch_mask)
+                    cv2.imwrite(root_directory + "256_patches/masks/" +
+                                mask_name + "patch_" + str(i) + str(j) + ".tif", single_patch_mask)
 
-train_img_dir = os.path.join(work_dir, "256_patches", "images")
-train_mask_dir = os.path.join(work_dir, "256_patches", "masks")
+train_img_dir = "data/256_patches/images/"
+train_mask_dir = "data/256_patches/masks/"
 
 img_list = os.listdir(train_img_dir)
 msk_list = os.listdir(train_mask_dir)
 
 num_images = len(os.listdir(train_img_dir))
 
+img_num = random.randint(0, num_images - 1)
 
-img_num = random.randint(0, num_images-1)
-
-img_for_plot = cv2.imread(os.path.join(train_img_dir, img_list[img_num]), 1)
+img_for_plot = cv2.imread(train_img_dir + img_list[img_num], 1)
 img_for_plot = cv2.cvtColor(img_for_plot, cv2.COLOR_BGR2RGB)
 
-mask_for_plot =cv2.imread(os.path.join(train_mask_dir, msk_list[img_num]), 0)
+mask_for_plot = cv2.imread(train_mask_dir + msk_list[img_num], 0)
 
 plt.figure(figsize=(12, 8))
 plt.subplot(121)
@@ -117,11 +143,12 @@ plt.title('Image')
 plt.subplot(122)
 plt.imshow(mask_for_plot, cmap='gray')
 plt.title('Mask')
-plt.savefig("figure02.png")
-plt.close()
+plt.show()
+
+###########################################################################
 
 # Now, let us copy images and masks with real information to a new folder.
-# real information = if mask has decent amount of labels other than 0.
+# real information = if mask has decent amount of labels other than 0. 
 
 useless = 0  # Useless image counter
 for img in range(len(img_list)):  # Using t1_list as all lists are of same size
@@ -129,16 +156,17 @@ for img in range(len(img_list)):  # Using t1_list as all lists are of same size
     mask_name = msk_list[img]
     print("Now preparing image and masks number: ", img)
 
-    temp_image = cv2.imread(os.path.join(train_img_dir, img_list[img]), 1)
-    temp_mask = cv2.imread(os.path.join(train_mask_dir, msk_list[img]), 0)
+    temp_image = cv2.imread(train_img_dir + img_list[img], 1)
+
+    temp_mask = cv2.imread(train_mask_dir + msk_list[img], 0)
     # temp_mask=temp_mask.astype(np.uint8)
 
     val, counts = np.unique(temp_mask, return_counts=True)
 
     if (1 - (counts[0] / counts.sum())) > 0.05:  # At least 5% useful area with labels that are not 0
         print("Save Me")
-        cv2.imwrite(os.path.join(work_dir, "256_patches", "images_with_useful_info/images/" + img_name), temp_image)
-        cv2.imwrite(os.path.join(work_dir, "256_patches", "images_with_useful_info/masks/" + mask_name), temp_mask)
+        cv2.imwrite('data/256_patches/images_with_useful_info/images/' + img_name, temp_image)
+        cv2.imwrite('data/256_patches/images_with_useful_info/masks/' + mask_name, temp_mask)
 
     else:
         print("I am useless")
@@ -147,7 +175,7 @@ for img in range(len(img_list)):  # Using t1_list as all lists are of same size
 print("Total useful images are: ", len(img_list) - useless)  # 20,075
 print("Total useless images are: ", useless)  # 21,571
 ###############################################################
-#Now split the data into training, validation and testing.
+# Now split the data into training, validation and testing.
 
 """
 Code for splitting folder into train, test, and val.
@@ -158,8 +186,47 @@ pip install split-folders
 """
 import splitfolders  # or import split_folders
 
-input_folder = os.path.join(work_dir, "256_patches", "images_with_useful_info")
-output_folder = os.path.join(work_dir, 'results')
+input_folder = 'data/256_patches/images_with_useful_info/'
+output_folder = 'data/data_for_training_and_testing/'
 # Split with a ratio.
 # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
-splitfolders.ratio(input_folder, output=output_folder, seed=42, ratio=(.75, .25), group_prefix=None) # default values
+splitfolders.ratio(input_folder, output=output_folder, seed=42, ratio=(.75, .25), group_prefix=None)  # default values
+########################################
+
+# Now manually move folders around to bring them to the following structure.
+"""
+Your current directory structure:
+Data/
+    train/
+        images/
+            img1, img2, ...
+        masks/
+            msk1, msk2, ....
+    val/
+        images/
+            img1, img2, ...
+        masks/
+            msk1, msk2, ....
+
+Copy the folders around to the following structure... 
+
+
+Data/
+    train_images/
+                train/
+                    img1, img2, img3, ......
+
+    train_masks/
+                train/
+                    msk1, msk, msk3, ......
+
+    val_images/
+                val/
+                    img1, img2, img3, ......                
+
+    val_masks/
+                val/
+                    msk1, msk, msk3, ......
+
+
+"""
