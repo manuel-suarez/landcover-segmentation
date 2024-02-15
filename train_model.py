c04376ddac6c6c1fb8efea225edeb8731afc8596
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-14 21:07:42 -0600

    Training

---
+++ train_model.py
@@ -0,0 +1,220 @@
+import os
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import segmentation_models as sm
+from tensorflow.keras.metrics import MeanIoU
+import random
+
+home_dir = os.path.expanduser('~')
+data_dir = os.path.join(home_dir, 'data')
+work_dir = os.path.join(data_dir, 'landcover-ai')
+
+train_img_dir = os.path.join(work_dir, 'results', 'train', 'images')
+train_mask_dir = os.path.join(work_dir, 'results', 'train', 'masks')
+
+img_list = os.listdir(train_img_dir)
+msk_list = os.listdir(train_mask_dir)
+
+num_images = len(os.listdir(train_img_dir))
+img_num = random.randint(0, num_images-1)
+
+img_for_plot = cv2.imread(train_img_dir+img_list[img_num], 1)
+img_for_plot = cv2.cvtColor(img_for_plot, cv2.COLOR_BGR2RGB)
+
+mask_for_plot =cv2.imread(train_mask_dir+msk_list[img_num], 0)
+
+plt.figure(figsize=(12, 8))
+plt.subplot(121)
+plt.imshow(img_for_plot)
+plt.title('Image')
+plt.subplot(122)
+plt.imshow(mask_for_plot, cmap='gray')
+plt.title('Mask')
+plt.savefig('figure03.png')
+plt.close()
+
+# Define data generator
+seed=24
+batch_size=16
+n_classes=4
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+from tensorflow.keras.utils import to_categorical
+
+BACKBONE = 'resnet34'
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+def preprocess_data(img, mask, num_class):
+    # Scale images
+    img = scaler.fit_transform(img.reshape(-1, img.shape[-1])).reshape(img.shape)
+    img = preprocess_input(img)
+    mask = to_categorical(mask, num_class)
+
+    return (img, mask)
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+def trainGenerator(train_img_path, train_mask_path, num_class):
+    img_data_gen_args = dict(horizontal_flip=True,
+                             vertical_flip=True,
+                             fill_mode='reflect')
+
+    image_datagen = ImageDataGenerator(**img_data_gen_args)
+    mask_datagen = ImageDataGenerator(**img_data_gen_args)
+
+    image_generator = image_datagen.flow_from_directory(
+        train_img_path,
+        class_mode=None,
+        batch_size=batch_size,
+        seed=seed)
+
+    mask_generator = mask_datagen.flow_from_directory(
+        train_mask_path,
+        class_mode=None,
+        color_mode='grayscale',
+        batch_size=batch_size,
+        seed=seed)
+
+    train_generator = zip(image_generator, mask_generator)
+
+    for (img, mask) in train_generator:
+        img, mask = preprocess_data(img, mask, num_class)
+        yield (img, mask)
+
+train_img_path = os.path.join(work_dir, 'results', 'train', 'images')
+train_mask_path = os.path.join(work_dir, 'results', 'train', 'masks')
+train_img_gen = trainGenerator(train_img_path, train_mask_path, num_class=4)
+
+val_img_path = os.path.join(work_dir, 'results', 'val', 'images')
+val_mask_path = os.path.join(work_dir, 'results', 'val', 'masks')
+val_img_gen = trainGenerator(val_img_path, val_mask_path, num_class=4)
+
+x, y = train_img_gen.__next__()
+for i in range(0,3):
+    image = x[i]
+    mask = np.argmax(y[i], axis=2)
+    plt.subplot(1,2,1)
+    plt.imshow(image)
+    plt.subplot(1,2,2)
+    plt.imshow(mask, cmap='gray')
+    plt.savefig(f"figure0{4+i}.png")
+    plt.close()
+
+x_val, y_val = val_img_gen.__next__()
+
+for i in range(0,3):
+    image = x_val[i]
+    mask = np.argmax(y_val[i], axis=2)
+    plt.subplot(1,2,1)
+    plt.imshow(image)
+    plt.subplot(1,2,2)
+    plt.imshow(mask, cmap='gray')
+    plt.savefig(f"figure0{7+i}.png")
+    plt.close()
+
+num_train_imgs = len(os.listdir(train_img_path))
+num_val_images = len(os.listdir(val_img_path))
+steps_per_epoch = num_train_imgs//batch_size
+val_steps_per_epoch = num_val_images//batch_size
+
+IMG_HEIGHT = x.shape[1]
+IMG_WIDTH  = x.shape[2]
+IMG_CHANNELS = x.shape[3]
+
+n_classes=4
+
+#############################################################################
+#Use transfer learning using pretrained encoder in the U-Net
+#(make sure you uncomment the preprocess_input part in the
+# preprocess_data function above)
+################################################################
+#Define the model
+# define model
+model = sm.Unet(BACKBONE, encoder_weights='imagenet',
+                input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS),
+                classes=n_classes, activation='softmax')
+model.compile('Adam', loss=sm.losses.categorical_focal_jaccard_loss, metrics=[sm.metrics.iou_score])
+
+#Other losses to try: categorical_focal_dice_loss, cce_jaccard_loss, cce_dice_loss, categorical_focal_loss
+
+#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=metrics)
+print(model.summary())
+print(model.input_shape)
+
+history=model.fit(train_img_gen,
+          steps_per_epoch=steps_per_epoch,
+          epochs=25,
+          verbose=1,
+          validation_data=val_img_gen,
+          validation_steps=val_steps_per_epoch)
+
+model.save('landcover_25_epochs_RESNET_backbone_batch16.hdf5')
+
+##################################################################
+#plot the training and validation IoU and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig("figure10.png")
+plt.close()
+
+acc = history.history['iou_score']
+val_acc = history.history['val_iou_score']
+
+plt.plot(epochs, acc, 'y', label='Training IoU')
+plt.plot(epochs, val_acc, 'r', label='Validation IoU')
+plt.title('Training and validation IoU')
+plt.xlabel('Epochs')
+plt.ylabel('IoU')
+plt.legend()
+plt.savefig("figure11.png")
+plt.close()
+
+#####################################################
+
+from keras.models import load_model
+
+model = load_model("landcover_25_epochs_RESNET_backbone_batch16.hdf5", compile=False)
+
+#batch_size=32 #Check IoU for a batch of images
+
+#Test generator using validation data.
+
+test_image_batch, test_mask_batch = val_img_gen.__next__()
+
+#Convert categorical to integer for visualization and IoU calculation
+test_mask_batch_argmax = np.argmax(test_mask_batch, axis=3)
+test_pred_batch = model.predict(test_image_batch)
+test_pred_batch_argmax = np.argmax(test_pred_batch, axis=3)
+
+n_classes = 4
+IOU_keras = MeanIoU(num_classes=n_classes)
+IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+#######################################################
+#View a few images, masks and corresponding predictions.
+img_num = random.randint(0, test_image_batch.shape[0]-1)
+
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_image_batch[img_num])
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(test_mask_batch_argmax[img_num])
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(test_pred_batch_argmax[img_num])
+plt.savefig("figure11.png")
+plt.close()
